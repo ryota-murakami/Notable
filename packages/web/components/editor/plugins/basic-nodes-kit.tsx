c1770b31e4c47142785bbbc2403c213d31802263
Ryota Murakami <dojce1048@gmail.com>
2025-08-05 07:04:23 +0900

    feat: enable rich content blocks in Plate.js editor
    - Add AdvancedBlocksKit to BasicNodesKit configuration
    - Includes code blocks, tables, media embeds, todo lists, toggles, callouts
    - Provides Notion-like rich content editing experience

--- packages/web/components/editor/plugins/basic-nodes-kit.tsx
+++ packages/web/components/editor/plugins/basic-nodes-kit.tsx
@@ -3,9 +3,11 @@
 import { BasicBlocksKit } from './basic-blocks-kit'
 import { BasicMarksKit } from './basic-marks-kit'
 import { WikiLinkPlugin } from './wiki-link-plate-plugin'
+import { AdvancedBlocksKit } from './advanced-blocks-kit'
 
 export const BasicNodesKit = [
   ...BasicBlocksKit,
   ...BasicMarksKit,
+  ...AdvancedBlocksKit,
   WikiLinkPlugin,
 ]
